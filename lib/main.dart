7716a7c98fa5d93ee14bd038783a41092c7358b6
Sharifjon <sharifboymuminov@gmail.com>
2025-02-07 08:52:25 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'screens/home_screen/home_screen.dart';
+
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(414, 896),
+      builder: (context, child) {
+        ScreenUtil.init(context);
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(useMaterial3: false),
+          home: child,
+        );
+      },
+      child: HomeScreen(),
+    );
+  }
+}
---
+++ lib/utils/app_text_style.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:warehouse_rvp/utils/app_colors.dart';
+
+class AppTextStyle {
+  // SeoulNamsan
+  static TextStyle nunitoThin = const TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w200,
+    color: AppColors.black,
+  );
+  static TextStyle nunitoLight = const TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w300,
+    color: AppColors.black,
+  );
+  static TextStyle nunitoRegular = const TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w400,
+    color: AppColors.black,
+  );
+  static TextStyle nunitoMedium = const TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+  );
+
+  static TextStyle nunitoSemiBold = const TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w600,
+    color: AppColors.black,
+  );
+
+  static TextStyle nunitoBold = const TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
+  );
+
+  static TextStyle nunitoBlack = const TextStyle(
+    fontFamily: "Nunito",
+    fontWeight: FontWeight.w800,
+    color: AppColors.black,
+  );
+
+}
